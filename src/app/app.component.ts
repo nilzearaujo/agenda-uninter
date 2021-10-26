02b2af3ce1fb1e693711b751e325178cb9df4c7a
nilzearaujo <nilzearaujo32@gmail.com>
2021-10-25 22:47:07 -0300

    Corrigido RU

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Aluno } from './model/arquivo';
 export class AppComponent {
   title = 'agenda';
   alunos = [
-    new Aluno('Nilze','1234','Analise e Desenvolvimento de sistemas', '12/01/1986'),
+    new Aluno('Nilzilene Simas de Araujo','3312120','Analise e Desenvolvimento de sistemas', '12/01/1986'),
     new Aluno('Gabriel','1319','Engenharia de Software', '13/11/1989'),
     new Aluno('Anne','1234','Analise e Desenvolvimento de sistemas', '12/05/1990'),
     new Aluno('Pamela','1234','Direito', '15/06/2000'),
